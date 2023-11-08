232ab8794d6585faa1c767e551d6c9f7a523b874
NadjaUst <example@gmail.com>
2023-11-08 13:55:18 +0100

    first version

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,15 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Information about UST company employees</h1>
+            <h2>Total number of employees: </h2>
+            <h2>Will receive a bonus: </h2>
+
+
+        </div>
+    )
+}
+
+export default AppInfo;
